42091cebe4dea8a592a433b08c32f857f3993e2c
Sanjib Narzary <o-._.-o@live.com>
2024-03-02 22:43:27 +0530

    Update lang_news.blade.php

--- resources/views/lang_news.blade.php
+++ resources/views/lang_news.blade.php
@@ -24,7 +24,7 @@
                     </div>
                 </div>
                 @endforeach
-                <div class="nav Pagination">
+                <div class="nav ">
                     {{ $news->links() }}
                 </div>
             </div>
