7516507620b6e2e05a4b0343694b22994edb6832
Sanjib Narzary <o-._.-o@live.com>
2024-03-04 06:32:56 +0530

    logo updates

--- resources/views/topic/lang_topic.blade.php
+++ resources/views/topic/lang_topic.blade.php
@@ -13,8 +13,8 @@
         <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://aisumnews.com">
-                    <!-- <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> //-->
-                    <h1 class="title is-4">AISumNews</h1>
+                    <img src="/images/logo.png" width="112" height="28"> 
+                    <!-- <h1 class="title is-4">AISumNews</h1> /-->
                 </a>
 
                 <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
