e0a389948b4afbfb92922d3dd2e02bba3ba81400
Sanjib Narzary <o-._.-o@live.com>
2024-03-04 22:49:49 +0530

    logo

--- resources/views/topic/lang_topic.blade.php
+++ resources/views/topic/lang_topic.blade.php
@@ -13,7 +13,7 @@
         <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://aisumnews.com">
-                    <img src="/images/icon-512.png" > 
+                    <img src="/images/home-logo.png" > 
                     <!-- <h1 class="title is-4">AISumNews</h1> /-->
                 </a>
 
