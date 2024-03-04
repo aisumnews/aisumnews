293bf71bdd5687493218f1b461ad4f99e0348a16
Sanjib Narzary <o-._.-o@live.com>
2024-03-04 07:05:30 +0530

    update logo with flutter_dash

--- resources/views/topic/lang_topic.blade.php
+++ resources/views/topic/lang_topic.blade.php
@@ -13,7 +13,7 @@
         <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://aisumnews.com">
-                    <img src="/images/logo.png" width="112" height="28"> 
+                    <img src="/images/icon-512.png" > 
                     <!-- <h1 class="title is-4">AISumNews</h1> /-->
                 </a>
 
