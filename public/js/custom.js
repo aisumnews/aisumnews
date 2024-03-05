1ffc8c6d544a9bd2a3715176dab262cefca2b3dd
Sanjib Narzary <o-._.-o@live.com>
2024-03-05 21:00:58 +0530

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $(".navbar-burger").click(function () {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
 
+
+  });
+  $(".navbar-menu").click(function () {
+    $(".navbar-menu").toggleClass("is-active");
   });
 });
