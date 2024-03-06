b07fd76b190df3830f5d588a88e65d7d1699c495
Sanjib Narzary <o-._.-o@live.com>
2024-03-06 13:54:22 +0530

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,6 +6,5 @@ $(document).ready(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
-
   });
 });
